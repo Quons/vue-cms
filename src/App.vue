82c9b934ba2b3b9ede440798d486283e5ae56bd9
quon <liuyc@knowbox.cn>
2019-04-11 11:14:17 +0800

    gdxg

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,41 @@
 <template>
-  <div id="app">
-    <!-- <router-link to="/transition2/transition9" tag="span">登录</router-link>
-    <router-link to="/transition11/test" tag="span">注册</router-link>-->
-    <router-view/>
-    <!-- <div style="display:flex;height:80px">
-      <router-view style="background-color:red;flex:8 " name="left"/>
-      <router-view name="right" style="background-color:green;flex:9"/>
-    </div>-->
+  <div id="app" class="app-container">
+    <!-- 顶部Heard -->
+    <mt-header fixed title="QUON BRAND"></mt-header>
+    <!-- 内容 -->
+    <!-- 底部栏 -->
+    <!-- <router-view/> -->
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-email" 
+          ><span class="mui-badge">9</span></span
+        >
+        <span class="mui-tab-label">消息</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">通讯录</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-gear"></span>
+        <span class="mui-tab-label">设置</span>
+      </a>
+    </nav>
   </div>
 </template>
 
 <script>
+import './lib/mui/css/mui.min.css'
 export default {
-  name: "App"
-};
+  name: 'App'
+}
 </script>
-<style>
-.myactive {
-  color: red;
-  font-weight: 800;
-  font-style: italic;
-  font-size: 80px;
-  text-decoration: underline;
-  background-color: green;
-}
-.v-enter,
-.v-leave-to {
-  opacity: 0;
-  transform: translateX(140px);
-}
-.v-enter-active,
-.v-leave-active {
-  transition: all 0.5s ease;
+<style lang="scss" scoped>
+.app-container {
+  padding: 40px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,42 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import MintUI from 'mint-ui'
-// import 'mint-ui/lib/style.css'
-import {Button} from 'mint-ui'
-// import vr from 'vue-resource'
 import axios from 'axios'
+import {Header} from 'mint-ui'
 Vue.config.productionTip = false
 // 设置http依赖库
 // 设置baseUrl
 axios.defaults.baseURL = 'http://localhost:9082/'
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
 Vue.prototype.$http = axios
-Vue.filter('dateformat', function (val) {
-  var value = new Date(val)
-  var year = value.getFullYear().toString().padStart(4, '0')
-  var month = (value.getMonth() + 1).toString().padStart(2, '0')
-  var day = value
-    .getDate()
-    .toString()
-    .padStart(2, '0')
-  var hour = value
-    .getHours()
-    .toString()
-    .padStart(2, '0')
-  var minutes = value
-    .getMinutes()
-    .toString()
-    .padStart(2, '0')
-  var seconds = value
-    .getSeconds()
-    .toString()
-    .padStart(2, '0')
-  return `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`
-})
 
-Vue.component(Button.name, Button);
-// Vue.use(MintUI);
+Vue.component(Header.name, Header);
 new Vue({ 
   el: '#app',
   router, 
